75169760960d7ece03484b225d13d7a51d6bad19
QwipeKun <moonsov@yandex.ru>
2022-01-19 20:47:45 +0300

    update acrive task

--- src/components/MainBlockOne/MainBlockOne.jsx
+++ src/components/MainBlockOne/MainBlockOne.jsx
@@ -3,7 +3,8 @@ import s from "./MainBlockOne.module.css";
 import iconSettings from "./../../svg/IconSettings.svg";
 import circle from "./../../svg/circle.svg";
 import score from "./../../svg/score.svg";
-import border from "./../../svg/border.svg"
+import border from "./../../svg/border.svg";
+import borderBlue from "./../../svg/borderBlue.svg"
 
 let MainBlockOne = () => {
   return (
@@ -91,29 +92,96 @@ let MainBlockOne = () => {
               <div className={s.newOrderTextContainer}>
                 <div className={s.newOrderText}>Новый заказ</div>
               </div>
-              <div className={s.orderNumberContainer}>
-                <div>Заказ #IQR000000289</div>
-                <div>г.Москва,ул. Коломенская, д.28/7</div>
-                <dvi>01.12.2021 - 18:30</dvi>
-              </div>
-              <div className={s.priceContainer}>
-                <div>
-                  <div>Цена:</div>
-                  <div>140 000,00 ₽</div>
-                </div>
-                <div>
-                  <div>
-                    <div>Количество SKU:</div>
-                    <div>300 шт</div>
+              <div className={s.containerOrderBlocks}>
+                <div className={s.conrainerO}>
+                  <div className={s.orderNumberContainer}>
+                    <div  className={s.boldValue}>Заказ #IQR000000289</div>
+                    <div>г.Москва,ул. Коломенская, д.28/7</div>
+                    <dvi>01.12.2021 - 18:30</dvi>
                   </div>
+                  <div className={s.priceContainer}>
+                    <div className={s.priceValueBlock}>
+                      <div className={s.priceValue}>Цена:</div>
+                      <div className={s.boldValue}>140 000,00 ₽</div>
+                    </div>
+                    <div>
+                      <div className={s.ValueBlock}>
+                        <div className={s.priceValue}>Количество SKU:</div>
+                        <div className={s.boldValue}>300 шт</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={s.borderSettings}>
+                  <img className={s.border} src={border} rel="border" />
                 </div>
               </div>
             </div>
-            <img className={s.border} src={border} rel="border" />
+          </div>
+          <div className={s.mainConinerOrder}>
+            <div className={s.mainBlocksOrder}>
+              <div className={s.newOrderTextContainerTwo}>
+                <div className={s.newOrderTextTwoBlock}>Заказ принят</div>
+              </div>
+              <div className={s.containerOrderBlocks}>
+                <div className={s.conrainerO}>
+                  <div className={s.orderNumberContainer}>
+                    <div  className={s.boldValue}>Заказ #IQR000000289</div>
+                    <div>г.Москва,ул. Коломенская, д.28/7</div>
+                    <dvi>01.12.2021 - 18:30</dvi>
+                  </div>
+                  <div className={s.priceContainer}>
+                    <div className={s.priceValueBlock}>
+                      <div className={s.priceValueBlue}>Цена:</div>
+                      <div className={s.boldValue}>140 000,00 ₽</div>
+                    </div>
+                    <div>
+                      <div className={s.ValueBlock}>
+                        <div className={s.priceValueBlue}>Количество SKU:</div>
+                        <div className={s.boldValue}>300 шт</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={s.borderSettings}>
+                  <img className={s.border} src={borderBlue} rel="borderBlue" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={s.mainConinerOrder}>
+            <div className={s.mainBlocksOrder}>
+              <div className={s.newOrderTextContainerTwo}>
+                <div className={s.newOrderTextTwoBlock}>Заказ принят</div>
+              </div>
+              <div className={s.containerOrderBlocks}>
+                <div className={s.conrainerO}>
+                  <div className={s.orderNumberContainer}>
+                    <div  className={s.boldValue}>Заказ #IQR000000289</div>
+                    <div>г.Москва,ул. Коломенская, д.28/7</div>
+                    <dvi>01.12.2021 - 18:30</dvi>
+                  </div>
+                  <div className={s.priceContainer}>
+                    <div className={s.priceValueBlock}>
+                      <div className={s.priceValueBlue}>Цена:</div>
+                      <div className={s.boldValue}>140 000,00 ₽</div>
+                    </div>
+                    <div>
+                      <div className={s.ValueBlock}>
+                        <div className={s.priceValueBlue}>Количество SKU:</div>
+                        <div className={s.boldValue}>300 шт</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className={s.borderSettings}>
+                  <img className={s.border} src={borderBlue} rel="borderBlue" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div>
-        </div>
+        <div></div>
       </div>
       <div></div>
       <div></div>
