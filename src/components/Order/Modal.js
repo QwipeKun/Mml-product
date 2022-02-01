3026edad1d8cba49390c50fe9aec0e0d9b04aa55
QwipeKun <moonsov@yandex.ru>
2022-02-01 14:42:16 +0300

    Modal added

--- src/components/Order/Modal.js
+++ src/components/Order/Modal.js
@@ -7,7 +7,8 @@ const Modal = props =>{
     return(
         <div className={`modal__wrapper ${props.isOpened ? 'open' : 'close'}`} style={{...props.style}}>
             <div className="modal__body">
-                <div className='modal__close' onClick={props.onModalClose}>x</div>
+                <div className='modal__name'>Активные заказы (4)</div>
+                <div className='modal__close' onClick={props.onModalClose}>Скрыть</div>
                 <h2>{props.title}</h2>
                 {props.children}
             </div>
--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -2,49 +2,49 @@ import s from "./Order.module.css";
 import border from "./../../svg/border.svg";
 import borderBlue from "./../../svg/borderBlue.svg";
 import OrderCart from "./OrderCart/OrderCart";
+import OrderCartYellow from "./OrderCartYellow/OrderCartYellow";
+import { useState } from "react";
+import Modal from "./Modal.js";
 
 let Order = () => {
+  const [modal, setModal] = useState({
+    modal1: false,
+    modal2: false,
+  });
+
   return (
     <div className={s.upperBlocksOrder}>
       <div>
         <div className={s.activeOrderBlock}>
           <div className={s.activeOrder}>Активные заказы (4)</div>
-          <button className={s.showAll}>Посмотреть все</button>
+          <button
+            className={s.showAll}
+            onClick={() =>
+              setModal({
+                ...modal,
+                modal1: true,
+              })
+            }
+          >
+            Посмотреть все
+          </button>
+          <Modal
+            closeTimeoutMS={200}
+            isOpened={modal.modal1}
+            onModalClose={() => setModal({ ...modal, modal1: false })}
+          >
+            <OrderCartYellow />
+            <OrderCart />
+            <OrderCart />
+            <OrderCart />
+          </Modal>
+          <Modal title={"Modal 2 Title "} isOpened={modal.modal2}>
+            <OrderCart />
+          </Modal>
         </div>
-        <div className={s.mainConinerOrder}>
-          <div className={s.mainBlocksOrder}>
-            <div className={s.newOrderTextContainer}>
-              <div className={s.newOrderText}>Новый заказ</div>
-            </div>
-            <div className={s.containerOrderBlocks}>
-              <div className={s.conrainerO}>
-                <div className={s.orderNumberContainer}>
-                  <div className={s.boldValue}>Заказ #IQR000000289</div>
-                  <div>г.Москва,ул. Коломенская, д.28/7</div>
-                  <dvi>01.12.2021 - 18:30</dvi>
-                </div>
-                <div className={s.priceContainer}>
-                  <div className={s.priceValueBlock}>
-                    <div className={s.priceValue}>Цена:</div>
-                    <div className={s.boldValue}>140 000,00 ₽</div>
-                  </div>
-                  <div>
-                    <div className={s.ValueBlock}>
-                      <div className={s.priceValue}>Количество SKU:</div>
-                      <div className={s.boldValue}>300 шт</div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className={s.borderSettings}>
-                <img className={s.border} src={border} rel="border" />
-              </div>
-            </div>
-          </div>
-        </div>
+        <OrderCartYellow />
         <OrderCart />
         <OrderCart />
-
       </div>
     </div>
   );
